7d59ca3872c8beffc3fb14c122fbe73a77a62663
김동섭 <84980169+Dong-Seob-Kim@users.noreply.github.com>
2021-06-24 17:44:16 +0900

    farmControl.jsp

--- github.com/2021-SMHRD-KDT-IoT-4/porkbeef-.git/Project/src/main/java/com/Front/front_Controller.java
+++ github.com/2021-SMHRD-KDT-IoT-4/porkbeef-.git/Project/src/main/java/com/Front/front_Controller.java
@@ -1,12 +1,15 @@
 package com.Front;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import com.controller.Command;
+import com.controller.GetActuatorStatusCon;
 import com.controller.GetDomesticAnimals;
 import com.controller.Login;
 
